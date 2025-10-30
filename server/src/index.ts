b3b9aba158d946de6cbf9ac6ee103486ab8cbb80
Drew Rodrigues <rodandrew95@gmail.com>
2025-10-30 12:03:01 -0700

    POC

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 const port = 10000;
 
+app.use(express.static("public"));
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  const spaFilePath = path.resolve(__dirname, "..", "public");
+  console.log(spaFilePath);
+  res.sendFile(spaFilePath);
+});
+
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
 });
 
 app.listen(port, () => {
