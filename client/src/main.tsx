66ba898622151828ac522e6d276034d442921fa1
Drew Rodrigues <rodandrew95@gmail.com>
2025-10-30 08:54:16 -0700

    Logs and import env var for API URL

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+if (import.meta.env.DEV) {
+  console.log("Is dev mode");
+  console.log(import.meta.env);
+} else {
+  console.log("Is prod mode");
+  console.log(import.meta.env);
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
