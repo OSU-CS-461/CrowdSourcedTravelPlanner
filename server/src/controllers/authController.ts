d1c4e0e6a85fe8ac3aba7d19eaa247195c7e0a56
Drew Rodrigues <rodandrew95@gmail.com>
2025-11-07 12:08:40 -0800

    Add baseline authController

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express";
 import { createUser } from "../services/userService";
+import { sign } from "../services/authTokenService";
+import { ZodError } from "zod";
 
 export const register = async (req: Request, res: Response) => {
-  const user = await createUser(req.body);
-  res.cookie("authToken", "<auth-token-here>", {
-    secure: process.env.NODE_ENV === "production", // TODO: make sure env vars are set correctly
-    httpOnly: true,
-  });
-  res.json(user);
+  try {
+    const user = await createUser(req.body);
+    const authToken = sign(user);
+    res.cookie("authToken", authToken, {
+      secure: process.env.NODE_ENV === "production", // TODO: make sure env vars are set correctly
+      httpOnly: true,
+    });
+    return res.status(201).json(user);
+  } catch (err) {
+    // TODO: add standard format for error messages
+    return res.status(400).json({ error: "Internal server error" });
+  }
 };
