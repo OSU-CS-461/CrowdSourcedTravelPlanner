35d9fd6e3cd681acca8489343a6f1c1427746612
Drew Rodrigues <rodandrew95@gmail.com>
2025-11-08 10:11:33 -0800

    WIP coverage

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vitest/config";
 
+const EXCLUDED_INTEGRATION_TEST_PATHS = [
+  "src/__tests__/controllers/**/*.tests.ts",
+  "src/__tests__/db/**/*.tests.ts",
+];
+
 export default defineConfig({
   test: {
     include: ["**/*.{tests,specs}.?(c|m)[jt]s?(x)"],
-    exclude: [
-      "src/__tests__/controllers/**/*.tests.ts",
-      "src/__tests__/db/**/*.tests.ts",
-    ],
+    exclude: [...EXCLUDED_INTEGRATION_TEST_PATHS],
     environment: "node",
     setupFiles: ["./vitest.setup.ts"],
+    coverage: {
+      include: ["src/models/**", "src/services/**"],
+    },
   },
 });
