45dc3e9d12db49dde017a01af8f9d06c0fc3d881
Drew Rodrigues <rodandrew95@gmail.com>
2025-11-09 15:50:31 -0800

    Remove cookie parser

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,16 +1,14 @@
 import express, { Request, Response } from "express";
 import path from "path";
-import { register } from "./controllers/authController";
+import { login, register } from "./controllers/authController";
 import { Routes } from "./routes";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware";
-// import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   const spaFilePath = path.resolve(__dirname, "..", "public");
