b3a68e42c4c186f0d25fe32f19f4f5c9fe8c1c8e
Drew Rodrigues <rodandrew95@gmail.com>
2025-11-10 06:51:44 -0800

    Add eslint for server

---
+++ server/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist", "coverage"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.jest,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+]);
