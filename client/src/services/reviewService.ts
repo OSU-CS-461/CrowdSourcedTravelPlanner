3395bf829d852129555249cceacf672bec71c9c2
travis-kuenzi <kuenzitravis@gmail.com>
2026-01-29 13:06:23 -0800

    Made a minimal change to fix linter error

--- client/src/services/reviewService.ts
+++ client/src/services/reviewService.ts
@@ -1,11 +1,18 @@
 const API = "/api";
 
-export async function getReviews(experienceId) {
+type ReviewId = string | number;
+type ReviewPayload = {
+  experienceId: ReviewId;
+  rating: number;
+  text: string;
+};
+
+export async function getReviews(experienceId: ReviewId) {
   const res = await fetch(`${API}/experiences/${experienceId}/reviews`);
   return res.json();
 }
 
-export async function createReview(data) {
+export async function createReview(data: ReviewPayload) {
   return fetch(`${API}/reviews`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -13,6 +20,6 @@ export async function createReview(data) {
   });
 }
 
-export async function deleteReview(id) {
+export async function deleteReview(id: ReviewId) {
   return fetch(`${API}/reviews/${id}`, { method: "DELETE" });
 }
